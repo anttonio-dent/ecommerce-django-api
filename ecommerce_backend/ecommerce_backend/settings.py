8bd9090740e3d516e5087bcec1a0cb01d14ca678
AntTonio Dent <anttoniodent@outlook.com>
2024-10-20 01:33:38 +0600

    auth app is added

--- ecommerce_backend/ecommerce_backend/settings.py
+++ ecommerce_backend/ecommerce_backend/settings.py
@@ -27,7 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'product_app',
+    'auth_app',
 ]
 
 MIDDLEWARE = [
--- ecommerce_backend/ecommerce_backend/urls.py
+++ ecommerce_backend/ecommerce_backend/urls.py
@@ -6,6 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('product_app.urls')),
+    path('api/auth/', include('auth_app.urls')),
+
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
