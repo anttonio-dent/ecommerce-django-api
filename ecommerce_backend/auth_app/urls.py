f0934219c362622ce64ae5cc82f7267dea6fddf7
AntTonio Dent <anttoniodent@outlook.com>
2024-10-21 07:55:14 +0600

    fixed url

--- ecommerce_backend/auth_app/urls.py
+++ ecommerce_backend/auth_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RegisterView, CustomTokenObtainPairView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('register', RegisterView.as_view(), name='register'),
+    path('login', CustomTokenObtainPairView.as_view(), name='login'),
 ]
